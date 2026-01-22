39ca610145caea1f37cf35909db6469220db5877
codex4jet <dss87jet@gmail.com>
2026-01-22 21:57:44 +0530

    Implement while loop for iteration

--- calculator.c
+++ calculator.c
@@ -1,32 +1,43 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    float a, b, d;
+
+    
+    float a,b,d;
     int c;
-    printf("1 for multiplication, 2 for division, 3 for sum, 4 for difference\n");
+    printf("1 for multiplication, 2 for division, 3 for sum , 4 for difference , 0 for exit \n ");
     printf("Enter first number: ");
-    scanf("%f", &a);
+    scanf("%f",&a);
     printf("Enter second number: ");
-    scanf("%f", &b);
+    scanf("%f",&b);
+    
+    while (c!=0){
     printf("Enter operation: ");
-    scanf("%d", &c);
+    scanf("%d",&c);
+
     
     if(c == 1 ){
         d = a*b;
-        printf("The product is %f", d);
+        printf("The product is %f\n", d);
+        
     }
     else if(c == 2){
         d = a/b;
-        printf("The quotient is %f", d);
+        printf("The quotient is %f\n", d);
+        
     }
     else if(c == 3){
         d = a+b;
-        printf("The sum is %f", d);
+        printf("The sum is %f\n", d);
+        
     }
-    else{
+    else if(c==4){
         d = a-b;
-        printf("The difference is %f", d);
+        printf("The difference is %f\n", d);
+        
     }
-    
-    return 0;
 }
+}
+
+       
