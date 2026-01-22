bf1e5d3fd7f596fc2d4dfb3a9b614873006e3b6a
Jinesh248 <jinesh.patel248@gmail.com>
2026-01-22 20:40:59 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
 int main() {
-
+    
+    float a,b,c,d;
+    printf("1 for multiplication, 2 for division, 3 for sum , 4 for difference\n");
+    printf("Enter first number: ");
+    scanf("%f",&a);
+    printf("Enter second number: ");
+    scanf("%f",&b);
+    printf("Enter operation: ");
+    scanf("%f",&c);
+    
+    if(c == 1 ){
+        d = a*b;
+        printf("The product is %f", d);
+    }
+    else if(c == 2){
+        d = a/b;
+        printf("The quotient is %f", d);
+    }
+    else if(c == 3){
+        d = a+b;
+        printf("The sum is %f", d);
+    }
+    else{
+        d = a-b;
+        printf("The difference is %f", d);
+    }
 }
