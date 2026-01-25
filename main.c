4d48fe5730d14ee4f2a58e09ba15ad9e77d3df6d
codex4jet <dipak01911@gmail.com>
2026-01-25 10:59:04 +0530

    fixed code for specific opration

--- main.c
+++ main.c
@@ -4,18 +4,29 @@
 int main() {
 
     
-    float a,b,d,e,f;
-    int c=-7;
-    printf("1 for multiplication\n, 2 for division\n, 3 for sum\n , 4 for difference\n 5 square\n 6 for cube \n ,7 log base 10 \n , 0 for exit \n ");
-    printf("Enter first number: ");
-    scanf("%f",&a);
-    printf("Enter second number: ");
-    scanf("%f",&b);
-    
-    while (c!=0){
-    printf("Enter operation: ");
+    float a,b,d,e,f,x;
+    int c=-1;
+    printf("-----------Simple Calculator---------- \n");
+    printf(" 1 for multiplication\n 2 for division\n 3 for sum\n 4 for difference\n 5 square\n 6 for cube \n 7 log base 10 \n 0 for exit \n ");
+    while(c!=0){
+    printf("Enter opration : ");
     scanf("%d",&c);
 
+    if(c==5 || c==6 || c==7){
+        printf("Enter the number :");
+        scanf("%f",&x);
+    }
+    else if(c<5 && c>0){
+        printf("Enter first number: ");
+        scanf("%f",&a);
+        printf("Enter second number: ");
+        scanf("%f",&b);
+    }
+    else {
+        printf("Enter valid choice  ! \n ");
+    }
+    
+    
     
     if(c == 1 ){
         d = a*b;
@@ -38,25 +49,20 @@ int main() {
         
     }
     else if(c==5){
-        printf("Square of %f is %f\n",a,sqrt(a));
-        printf("Square of %f is %f",b,sqrt(b));
-
+        printf("Square of %f is : %f\n",x,x*x);
+        
     }
     else if(c==6){
-        printf("Cube of %f is %f\n",a,cbrt(a));
-        printf("cube of %f is %f\n",a,cbrt(b));
-
+        printf("Cube of %f is %f\n",x,x*x*x);
+        
     }
     else if(c==7){
-        printf("log  of %f is %f\n",a,log10(a));
-        printf("log  of %f is %f\n",a,log10(b));
-
-    }
-    
-    else {
-        printf("Enter valid choice  ! \n ");
+        printf("log  of %f is %f\n",x,log10(x));
+        
     }
 }
+    
+    
 }
 
        
